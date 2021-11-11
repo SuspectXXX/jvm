0bc7aaa7eda30bdc3b4cddafc42ebc0c05ba6ee9
kaige.yuan <y13262639708@163.com>
2021-11-11 16:18:02 +0800

    PC寄存器的测试代码

---
+++ PCRegister/src/com/kk/java/PCRegisterTest.java
@@ -0,0 +1,13 @@
+package com.kk.java;
+
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+
+        String s = "kk";
+        System.out.println(s);
+        System.out.println(k);
+    }
+}

011fc844e7800865d6ea60ec4c049816d03b539f
kaige.yuan <y13262639708@163.com>
2021-11-12 16:00:21 +0800

    演示Java虚拟机栈中栈帧内部的本地变量表相关内容

--- JavaStack/src/com/kk/java/JavaStackTest.java
+++ JavaStack/src/com/kk/java/JavaStackTest.java
@@ -1,9 +1,16 @@
 package com.kk.java;
 
 public class JavaStackTest {
+
+    private int num = 10;
+
     public static void main(String[] args) {
         JavaStackTest javaStackTest = new JavaStackTest();
         int num = 10;
         System.out.println("main");
+    }
+
+    public int method1() {
+        return 1;
     }
 }

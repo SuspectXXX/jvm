743f386be83eb49e55c6a3e96b8e0645fef7970a
kaige.yuan <y13262639708@163.com>
2021-11-14 21:19:29 +0800

    演示Java虚拟机中堆中GC日志分析

---
+++ Heap/src/com/kk/java/GCTest.java
@@ -0,0 +1,13 @@
+package com.kk.java;
+
+public class GCTest {
+
+    private static final int _1MB = 1024 * 1024;
+
+    public static void main(String[] args) {
+        byte[] alloction1 = new byte[2 * _1MB];
+        byte[] alloction2 = new byte[2 * _1MB];
+        byte[] alloction3 = new byte[2 * _1MB];
+        byte[] alloction4 = new byte[2 * _1MB];
+    }
+}
